8720d37bc8b948ffbb6a73704a34efb11f1d2130
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2023-11-23 18:01:00 +0200

    testing

--- m1p2.s
+++ m1p2.s
@@ -1,40 +1,44 @@
-.section .data
-printf_format: .asciz "%x\n"
-
 .section .text
 .global m1p2
+.type m1p2, %function
 
 m1p2:
-    push {r4, r5, r6, r7, r11, lr}
+    push {r4, r5, r6, r7, lr}
 
     mov r4, #1
     mov r5, r0 // Copy N into r5
 
-.loop:
-    mov r7, r4
+main_loop:
     mov r6, #0 // Reset counter
-.shift_loop:
+    mov r7, r4
+
+bit_count_loop:
     cmp r7, #0 // Check if we reached 0
-    beq .shifting_done
+    beq check_bit_count
 
     tst r7, #1 // Check if last bit is 1
     addne r6, r6, #1 // increment ones counter, if result of AND is non zero
 
-    mov r7, r7, LSR#1 // Right shift current value
-    b .shift_loop
-.shifting_done:
-    cmp r6, #3 // Check if counter of ones reached 3
-    bne .skip_print
+    lsr r7, r7, #1 // Right shift current value
+    b bit_count_loop
 
-    ldr r0, adr_printf_format
+check_bit_count:
+    cmp r6, #3 // Check if counter of ones reached 3
+    bne increment_loop_counter
+
+    ldr r0, =format_string
     mov r1, r4
     bl printf
 
-.skip_print:
+increment_loop_counter:
+    add r4, r4, #1
     cmp r4, r5
-    addlt r4, r4, #1
-    bne .loop
-exit:
-    pop {r4, r5, r6, r7, r11, pc}
+    ble main_loop
 
-adr_printf_format: .word printf_format
+exit:
+    pop {r4, r5, r6, r7, lr}
+    bx lr
+
+.section .rodata
+format:
+    .asciz "%x\n"
